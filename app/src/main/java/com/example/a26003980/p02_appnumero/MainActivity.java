dcd2cff082892fe6b3a4f637a3836386fb2e724f
Manoel S. Costa <ManoelSCosta>
2020-03-04 20:53:20 -0300

    metodos, e a execução

--- app/src/main/java/com/example/a26003980/p02_appnumero/MainActivity.java
+++ app/src/main/java/com/example/a26003980/p02_appnumero/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.a26003980.p02_appnumero;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -27,6 +28,31 @@ public class MainActivity extends AppCompatActivity {
         txtImpares = findViewById(R. id.txt_impares);
         txtPares = findViewById(R. id.txt_pares);
         txtSoma = findViewById(R. id.txt_soma);
-        
-    }
-}
+
+
+    }// fim do onCreate
+
+    //4 Criação do metodo quando o botão for tocado
+    public void processar(View v){
+        //5. pegar os numeros digitados
+        String numeroDigitado = campoNumero.getText().toString();
+        //6 conversão de string para int
+        int numero = Integer.parseInt(numeroDigitado);
+        //7 armazenar na lista
+        lista.add(numero);
+        //8 verificar se é par ou impar
+        if (numero % 2 == 0)
+            qtdPares++;
+        else
+            qtdImpares++;
+        //9 realizar a soma do numeros
+        soma = soma + numero;
+        //10 atribuir os resultados nos campos respectivos
+        txtNumeros.setText(lista.toString());
+        txtImpares.setText(qtdImpares + "");
+        txtPares.setText(qtdPares + "");
+        txtSoma.setText(soma + "");
+
+    }// fim do processar
+
+}// fim da classe
